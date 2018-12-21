c86517e560cb767ee2d056812691bd471b10486e
杨帆 <15234408101@163.com>
2018-12-21 17:34:26 +0800

    Update interceptor.js

--- ajaxhook/interceptor.js
+++ ajaxhook/interceptor.js
@@ -1,37 +1,39 @@
-; (function () {
-    let cacheXHR = XMLHttpRequest;
-    window.cacheXHR = cacheXHR;
+;(function() {
+  let cacheXHR = XMLHttpRequest;
+  window.cacheXHR = cacheXHR;
 
-    window.xAajax = function (proxyHook) {
-        XMLHttpRequest = function () {
-            let xhr = new cacheXHR();
-            var self = this;
-            for (let attr in xhr) {
-                let val = xhr[attr];
-                if (typeof val === "function") {
-                    this[attr] = function () {
-                        
-                        val.apply(xhr, [...arguments]);
-                    };
-                } else {
-                    Object.defineProperty(this, attr, {
-                        set(v) {
-                            if (typeof v === "function") {
-                                xhr[attr]=function () {
-                                    if(typeof proxyHook[attr] === "function" && proxyHook[attr].apply(xhr, [...arguments])) return;
-                                    v.apply(xhr, [...arguments]);
-                                };
-                            } else {
-                                xhr[attr]=v;
-                            }
-                        },
-                        get() {
-                            return xhr[attr];
-                        }
-                    });
-                }
+  window.xAjax = function(proxyHook) {
+    XMLHttpRequest = function() {
+      let xhr = new cacheXHR();
+      var self = this;
+      for (let attr in xhr) {
+        let val = xhr[attr];
+        if (typeof val === "function") {
+          this[attr] = function() {
+            val.apply(xhr, [...arguments]);
+          };
+        } else {
+          Object.defineProperty(this, attr, {
+            set(v) {
+              if (typeof v === "function") {
+                xhr[attr] = function() {
+                  if (
+                    typeof proxyHook[attr] === "function" &&
+                    proxyHook[attr](xhr)
+                  )
+                    return;
+                  v.apply(xhr, [...arguments]);
+                };
+              } else {
+                xhr[attr] = v;
+              }
+            },
+            get() {
+              return xhr[attr];
             }
-        };
-    }
-
-}());
+          });
+        }
+      }
+    };
+  };
+})();
